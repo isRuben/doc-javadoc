7dd9b2c270b2d7d2948de20d2756f47557f5c77f
Ruben <dama608@vidalibarraquer.net>
2024-04-19 10:55:23 +0200

    ASD

---
+++ p1/src/main/java/com/example/Hotel.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Hotel {
+    
+    private String name;
+    private String address;
+    private String web;
+    private enum HotelCategory {
+        OneStar, TwoStar, ThreeStar, FourStar, FiveStar;
+    }
+}
